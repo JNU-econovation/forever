d381b69cff85b44ebd387fa284122618768fb0f3
HyeonjiJang <jhg050908@gmail.com>
2024-04-02 13:55:00 +0900

    feat: Navigation Graph 정의

--- AOS/app/src/main/java/com/fourever/forever/navigation/ForeverNavGraph.kt
+++ AOS/app/src/main/java/com/fourever/forever/navigation/ForeverNavGraph.kt
@@ -1,2 +1,19 @@
 package com.fourever.forever.navigation
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.fourever.forever.presentation.home.HomeScreen
+
+@Composable
+fun ForeverNavGraph() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = Screen.Home.route) {
+        composable(Screen.Home.route) {
+            HomeScreen()
+        }
+        detailGraph(navController = navController)
+        generationGraph(navController = navController)
+    }
+}
---
+++ AOS/app/src/main/java/com/fourever/forever/navigation/GenerationNestedGraph.kt
@@ -0,0 +1,25 @@
+package com.fourever.forever.navigation
+
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import androidx.navigation.navigation
+import com.fourever.forever.presentation.fileupload.FileUploadScreen
+import com.fourever.forever.presentation.generatequestion.GenerateQuestionScreen
+import com.fourever.forever.presentation.generatesummary.GenerateSummaryScreen
+
+fun NavGraphBuilder.generationGraph(navController: NavController) {
+    navigation(
+        startDestination = Screen.UploadFile.route, route = Screen.Generation.route
+    ) {
+        composable(Screen.UploadFile.route) {
+            FileUploadScreen()
+        }
+        composable(Screen.GenerateSummary.route) {
+            GenerateSummaryScreen()
+        }
+        composable(Screen.GenerateQuestion.route) {
+            GenerateQuestionScreen()
+        }
+    }
+}
