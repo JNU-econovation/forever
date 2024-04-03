51a5e2a92454f552aa952b0ac6f4a67851c07cd8
MinjuKwak01 <mkwak1125@gmail.com>
2024-04-03 14:22:57 +0900

    feat: Question 엔티티 생성

---
+++ BE/forever/src/main/java/com/example/forever/domain/Question.java
@@ -0,0 +1,26 @@
+package com.example.forever.domain;
+
+import com.example.forever.domain.Document;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="question_tb")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 45, nullable = false)
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "document_id", nullable = false)
+    private Document document;
+
+}
+
