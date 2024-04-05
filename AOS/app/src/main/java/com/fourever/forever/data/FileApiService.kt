4be55c86823ae11f30e4e70553b8b4a602227685
HyeonjiJang <jhg050908@gmail.com>
2024-04-05 17:10:57 +0900

    feat: 서버와 통신을 위한 ApiService 인터페이스 생성

---
+++ AOS/app/src/main/java/com/fourever/forever/data/FileApiService.kt
@@ -0,0 +1,48 @@
+package com.fourever.forever.data
+
+import com.fourever.forever.data.model.BaseResponse
+import com.fourever.forever.data.model.request.PostFileQuestionRequestDto
+import com.fourever.forever.data.model.request.PostFileSummaryRequestDto
+import com.fourever.forever.data.model.response.GetFileListResponseDto
+import com.fourever.forever.data.model.response.GetFileQuestionResponseDto
+import com.fourever.forever.data.model.response.GetFileSummaryResponseDto
+import com.fourever.forever.data.model.response.GetQuestionListDto
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface FileApiService {
+    @GET("/api/documents")
+    suspend fun getFileList(
+        @Query(value = "page") page: Int,
+    ): Result<BaseResponse<GetFileListResponseDto>>
+
+    @POST("/api/documents/summary")
+    suspend fun postFileSummary(
+        @Body postFileSummaryRequestDto: PostFileSummaryRequestDto
+    ): Result<BaseResponse<Void>>
+
+    @POST("/api/documents/{documentId}/questions")
+    suspend fun postFileQuestion(
+        @Path(value = "documentId") documentId: Int,
+        @Body postFileQuestionRequestDto: PostFileQuestionRequestDto
+    ): Result<BaseResponse<Void>>
+
+    @GET("/api/documents/{documentId}/summary")
+    suspend fun getFileSummary(
+        @Path(value = "documentId") documentId: Int,
+    ): Result<BaseResponse<GetFileSummaryResponseDto>>
+
+    @GET("/api/documents/{documentId}/questions")
+    suspend fun getFileQuestionList(
+        @Path(value = "documentId") documentId: Int,
+    ): Result<BaseResponse<GetQuestionListDto>>
+
+    @GET("/api/documents/{documentId}/questions/{questionId}")
+    suspend fun getFileQuestion(
+        @Path(value = "documentId") documentId: Int,
+        @Path(value = "questionId") questionId: Int,
+    ): Result<BaseResponse<GetFileQuestionResponseDto>>
+}
