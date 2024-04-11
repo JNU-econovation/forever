72bd5630d431ec806f8b81061e0b89cd30c1e11d
HyeonjiJang <jhg050908@gmail.com>
2024-04-11 11:04:03 +0900

    fix: NavigationRoute 수정 - Navigation Argument 추가

--- AOS/app/src/main/java/com/fourever/forever/navigation/DetailNestedGraph.kt
+++ AOS/app/src/main/java/com/fourever/forever/navigation/DetailNestedGraph.kt
@@ -2,7 +2,9 @@ package com.fourever.forever.navigation
 
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavType
 import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
 import androidx.navigation.navigation
 import com.fourever.forever.presentation.getquestion.GetQuestionScreen
 import com.fourever.forever.presentation.getsummary.GetSummaryScreen
@@ -11,10 +13,32 @@ fun NavGraphBuilder.detailGraph(navController: NavController) {
     navigation(
         startDestination = Screen.GetSummary.route, route = Screen.Detail.route
     ) {
-        composable(Screen.GetSummary.route) {
+        composable(
+            Screen.GetSummary.route,
+            arguments = listOf(
+                navArgument(ForeverDestinationArgs.DOCUMENT_ID_ARG) {
+                    type = NavType.IntType
+                    defaultValue = 0
+                }
+            )
+        ) {
+            val documentId = it.arguments?.getBoolean(ForeverDestinationArgs.DOCUMENT_ID_ARG) ?: 0
+
             GetSummaryScreen("", "")
         }
-        composable(Screen.GetQuestion.route) {
+        composable(
+            Screen.GetQuestion.route,
+            arguments = listOf(
+                navArgument(ForeverDestinationArgs.DOCUMENT_ID_ARG) {
+                    type = NavType.IntType
+                    defaultValue = 0
+                },
+                navArgument("fileName") {
+                    type = NavType.StringType
+                    defaultValue = ""
+                }
+            )
+        ) {
             GetQuestionScreen()
         }
     }
