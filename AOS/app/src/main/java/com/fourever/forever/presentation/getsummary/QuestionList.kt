4e4186e56f71cdf9722801618367e513d03688a9
HyeonjiJang <jhg050908@gmail.com>
2024-04-18 10:51:01 +0900

    update: 질문 목록 UI 구성 변경 - 중복되는 Divider 제거

--- AOS/app/src/main/java/com/fourever/forever/presentation/getsummary/QuestionList.kt
+++ AOS/app/src/main/java/com/fourever/forever/presentation/getsummary/QuestionList.kt
@@ -5,15 +5,20 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Divider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.fourever.forever.R
 import com.fourever.forever.data.model.response.GetQuestionListDto
+import com.fourever.forever.presentation.component.DIVIDER_THICKNESS
+import com.fourever.forever.presentation.component.DIVIDER_WIDTH
 import com.fourever.forever.presentation.component.Question
 import com.fourever.forever.presentation.component.btmsheet.BTM_SHEET_CONTENT_VERTICAL_PADDING
 import com.fourever.forever.presentation.component.btmsheet.BTM_SHEET_SPACE_BETWEEN_TITLE_AND_CONTENT
@@ -35,6 +40,11 @@ fun QuestionList(
                 )
             }
         }
+        Divider(
+            modifier = Modifier.width(DIVIDER_WIDTH.dp),
+            color = colorResource(R.color.secondary_medium),
+            thickness = DIVIDER_THICKNESS.dp
+        )
         Spacer(modifier = Modifier.height(BTM_SHEET_CONTENT_VERTICAL_PADDING.dp))
         LongColorBtn(
             text = stringResource(id = R.string.question_list_check_question_button),
