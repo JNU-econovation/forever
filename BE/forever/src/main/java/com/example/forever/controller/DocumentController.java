c25455c64d2fb94b6e3fbe111e7a79a1df68f8e2
MinjuKwak01 <mkwak1125@gmail.com>
2024-04-27 00:04:03 +0900

    feat : 요약 저장 controller

---
+++ BE/forever/src/main/java/com/example/forever/controller/DocumentController.java
@@ -0,0 +1,25 @@
+package com.example.forever.controller;
+
+import com.example.forever.service.DocumentService;
+import com.example.forever.common.ApiResponse;
+import com.example.forever.common.ApiResponseGenerator;
+import com.example.forever.common.ErrorCode;
+import com.example.forever.dto.DocumentSummaryRequest;
+import com.example.forever.dto.DocumentSummaryResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class DocumentController {
+
+    private final DocumentService documentService;
+
+    @PostMapping("/documents/summary")
+    public ApiResponse<ApiResponse.SuccesCustomBody<DocumentSummaryResponse>> saveDocumentSummary(@RequestBody DocumentSummaryRequest request){
+        DocumentSummaryResponse response = documentService.saveDocumentSummary(request);
+        return ApiResponseGenerator.success(response, HttpStatus.OK, ErrorCode.SUCCESS);
+    }
+}
