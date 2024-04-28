5dc9f39e3ec8efbce2374f01d421430bd9079a81
MinjuKwak01 <mkwak1125@gmail.com>
2024-04-28 22:31:16 +0900

    feat : answer 도메인 builder 작성

--- BE/forever/src/main/java/com/example/forever/domain/Answer.java
+++ BE/forever/src/main/java/com/example/forever/domain/Answer.java
@@ -2,6 +2,7 @@ package com.example.forever.domain;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -20,5 +21,11 @@ public class Answer {
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "question_id", nullable = false)
     private Question question;
+
+    @Builder
+    public Answer(String content, Question question) {
+        this.content = content;
+        this.question = question;
+    }
 
 }
