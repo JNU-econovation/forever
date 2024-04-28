cb1ffa4a8355078882eef37d30ae41e4eec6531b
MinjuKwak01 <mkwak1125@gmail.com>
2024-04-28 22:30:56 +0900

    feat : 파일별 질문답변쌍 controller

--- BE/forever/src/main/java/com/example/forever/controller/DocumentController.java
+++ BE/forever/src/main/java/com/example/forever/controller/DocumentController.java
@@ -1,9 +1,9 @@
 package com.example.forever.controller;
 
+import com.example.forever.dto.SaveQuestionAnswerRequest;
 import com.example.forever.service.DocumentService;
 import com.example.forever.common.ApiResponse;
 import com.example.forever.common.ApiResponseGenerator;
-import com.example.forever.common.ErrorCode;
 import com.example.forever.dto.DocumentSummaryRequest;
 import com.example.forever.dto.DocumentSummaryResponse;
 import lombok.RequiredArgsConstructor;
@@ -12,14 +12,20 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api")
+@RequestMapping("/api/documents")
 public class DocumentController {
 
     private final DocumentService documentService;
 
-    @PostMapping("/documents/summary")
-    public ApiResponse<ApiResponse.SuccesCustomBody<DocumentSummaryResponse>> saveDocumentSummary(@RequestBody DocumentSummaryRequest request){
+    @PostMapping("/summary")
+    public ApiResponse<ApiResponse.SuccesCustomBody<DocumentSummaryResponse>> saveDocumentSummary(@RequestBody DocumentSummaryRequest request) {
         DocumentSummaryResponse response = documentService.saveDocumentSummary(request);
         return ApiResponseGenerator.success(response, HttpStatus.OK);
+    }
+
+    @PostMapping("/{documentId}/questions")
+    public ApiResponse<ApiResponse.SuccesCustomBody<Void>> saveDocumentQuestionAndAnswer(@PathVariable("documentId") Long documentId, @RequestBody SaveQuestionAnswerRequest request){
+        documentService.saveDocumentQuestionAndAnswer(documentId, request);
+        return ApiResponseGenerator.success(HttpStatus.OK);
     }
 }
