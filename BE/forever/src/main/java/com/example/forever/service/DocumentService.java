3c4b2d5827f9bc1a49816e4ef0b86984ae2c47f6
MinjuKwak01 <mkwak1125@gmail.com>
2024-04-29 22:51:13 +0900

    feat : 질문 답변쌍 조회 service로직 작성

--- BE/forever/src/main/java/com/example/forever/service/DocumentService.java
+++ BE/forever/src/main/java/com/example/forever/service/DocumentService.java
@@ -60,9 +60,24 @@ public class DocumentService {
         List<EachQuestionResponse> responses = questionList.stream()
                 .map(QuestionConversion::convertToEachQuestionResponse)
                 .toList();
-        
+
         return new QuestionListResponse(responses);
     }
+
+    public QuestionAnswerResponse getQuestionAndAnswer(Long questionId) {
+      //  Question question = questionRepository.findById(questionId)
+       //         .orElseThrow(() -> new IllegalArgumentException("Question not found " + questionId));
+
+        Answer answer = answerRepository.findByQuestionId(questionId).orElseThrow(
+                () -> new IllegalArgumentException("Answer not found " + questionId)
+        );
+
+        Question question = answer.getQuestion();
+
+        return new QuestionAnswerResponse(question.getContent(), answer.getContent());
+    }
+    //여기서 조회를 2번해서 가져올 수 있지만 서로 연관되어있는 question, answer를 한번에 가져오기 위해 join fetch를 사용하여 조회를 한번만 하도록
+    //하지만 이렇게 하면 questionId의 값이 없을 경우에는 예외가 발생하게 되므로 Optional로 감싸서 반환하도록
 
 }
 
