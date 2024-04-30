bf939fb1b720c15b83f2da374e677c816a6e00d7
HyeonjiJang <jhg050908@gmail.com>
2024-04-30 11:44:33 +0900

    feat: GetAllQuestionScreen 화면 및 기능 구현

--- AOS/app/src/main/java/com/fourever/forever/navigation/DetailNestedGraph.kt
+++ AOS/app/src/main/java/com/fourever/forever/navigation/DetailNestedGraph.kt
@@ -9,8 +9,10 @@ import androidx.navigation.NavType
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
 import androidx.navigation.navigation
-import com.fourever.forever.presentation.getquestion.GetQuestionViewModel
+import com.fourever.forever.presentation.getquestion.GetAllQuestionScreen
+import com.fourever.forever.presentation.getquestion.GetAllQuestionViewModel
 import com.fourever.forever.presentation.getquestion.GetSingleQuestionScreen
+import com.fourever.forever.presentation.getquestion.GetSingleQuestionViewModel
 import com.fourever.forever.presentation.getsummary.GetSummaryScreen
 import com.fourever.forever.presentation.getsummary.GetSummaryViewModel
 
@@ -75,15 +77,15 @@ fun NavGraphBuilder.detailGraph(
             )
         ) {
             /* TODO: 동알한 ViewModel을 쓰는 다른 두 Screen은 그 ViewModel을 반드시 공유해야 할까? */
-            val getQuestionViewModel = hiltViewModel<GetQuestionViewModel>()
-            val questionUiState by getQuestionViewModel.questionUiState.collectAsState()
+            val getQuestionViewModel = hiltViewModel<GetSingleQuestionViewModel>()
+            val questionUiState by getQuestionViewModel.singleQuestionUiState.collectAsState()
 
             val documentId = it.arguments?.getInt(ForeverDestinationArgs.DOCUMENT_ID_ARG) ?: 0
             val questionId = it.arguments?.getInt(ForeverDestinationArgs.QUESTION_ID_ARG) ?: 0
             val fileName = it.arguments?.getString(ForeverDestinationArgs.FILE_NAME_ARG) ?: ""
 
             GetSingleQuestionScreen(
-                questionUiState = questionUiState,
+                singleQuestionUiState = questionUiState,
                 fileName = fileName,
                 getQuestion = { (getQuestionViewModel::getQuestion)(documentId, questionId) },
                 navigateUp = { navController.navigateUp() }
@@ -107,7 +109,22 @@ fun NavGraphBuilder.detailGraph(
                 }
             )
         ) {
-            // GetAllQuestionScreen()
+            val getQuestionViewModel = hiltViewModel<GetAllQuestionViewModel>()
+            val allQuestionUiState by getQuestionViewModel.allQuestionUiState.collectAsState()
+
+            val documentId = it.arguments?.getInt(ForeverDestinationArgs.DOCUMENT_ID_ARG) ?: 0
+            val fileName = it.arguments?.getString(ForeverDestinationArgs.FILE_NAME_ARG) ?: ""
+            val questionSize = it.arguments?.getInt(ForeverDestinationArgs.QUESTION_SIZE_ARG) ?: 0
+
+            GetAllQuestionScreen(
+                allQuestionUiState = allQuestionUiState,
+                fileName = fileName,
+                questionSize = questionSize,
+                getQuestion = { questionIndex -> (getQuestionViewModel::getQuestion)(documentId, questionIndex) },
+                navigateUpToSummary = { navController.navigateUp() },
+                navigateUpToPrevQuestion = { navController.navigateUp() },
+                updateQuestionIndex = { getQuestionViewModel.updateQuestionIndex() }
+            )
         }
     }
 }
---
+++ AOS/app/src/main/java/com/fourever/forever/presentation/getquestion/GetAllQuestionViewModel.kt
@@ -0,0 +1,70 @@
+package com.fourever.forever.presentation.getquestion
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.fourever.forever.data.ResultWrapper
+import com.fourever.forever.data.repository.FileRepository
+import com.fourever.forever.presentation.util.UiState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+data class AllQuestionUiState(
+    val questionState: UiState = UiState.Empty,
+    val errorMessage: String = "",
+
+    val questionIndex: Int = 1,
+    val question: String = "",
+    val answer: String = ""
+)
+
+@HiltViewModel
+class GetAllQuestionViewModel @Inject constructor(
+    private val fileRepository: FileRepository
+) : ViewModel() {
+    private val _allQuestionUiState = MutableStateFlow(AllQuestionUiState())
+    val allQuestionUiState: StateFlow<AllQuestionUiState> = _allQuestionUiState.asStateFlow()
+
+
+    fun getQuestion(documentId: Int, questionId: Int) {
+        viewModelScope.launch {
+            fileRepository.getFileQuestion(documentId, questionId)
+                .onStart { _allQuestionUiState.update { it.copy(questionState = UiState.Loading) }}
+                .collect { result ->
+                    if (result is ResultWrapper.Success) {
+                        result.data.let { result ->
+                            _allQuestionUiState.update {
+                                it.copy(
+                                    questionState = UiState.Success,
+                                    question = result.data?.content ?: "",
+                                    answer = result.data?.answer ?: ""
+                                )
+                            }
+                        }
+                    } else if (result is ResultWrapper.Error) {
+                        _allQuestionUiState.update {
+                            it.copy(
+                                questionState = UiState.Failure,
+                                errorMessage = result.errorMessage,
+                                question = "",
+                                answer = ""
+                            )
+                        }
+                    }
+                }
+        }
+    }
+
+    fun updateQuestionIndex() {
+        _allQuestionUiState.update {
+            it.copy(
+                questionIndex = _allQuestionUiState.value.questionIndex + 1
+            )
+        }
+    }
+}
