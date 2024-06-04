2dd08ca71a9a0f56406a17443b9c2d11bd1ce631
HyeonjiJang <jhg050908@gmail.com>
2024-06-04 09:27:43 +0900

    feat: 요약 생성 시 로딩 화면 표시

--- AOS/app/src/main/java/com/fourever/forever/presentation/generatesummary/GenerateSummaryScreen.kt
+++ AOS/app/src/main/java/com/fourever/forever/presentation/generatesummary/GenerateSummaryScreen.kt
@@ -9,11 +9,8 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -30,7 +27,6 @@ import com.fourever.forever.presentation.component.buttons.ShortWhiteBtn
 import com.fourever.forever.ui.theme.foreverTypography
 
 private const val SPACE_BETWEEN_TITLE_AND_CONTENT = 50
-private const val CONTENT_AREA_HEIGHT = 550
 private const val SPACE_BETWEEN_CONTENT_AND_BUTTONS = 45
 
 @Composable
@@ -72,19 +68,7 @@ fun GenerateSummaryScreen(
                     )
                 }
                 Spacer(modifier = Modifier.size(SPACE_BETWEEN_TITLE_AND_CONTENT.dp))
-                Column(
-                    modifier = Modifier
-                        .height(
-                            height = CONTENT_AREA_HEIGHT.dp
-                        )
-                        .verticalScroll(rememberScrollState())
-                ) {
-                    Text(
-                        text = generateSummaryUiState.summary,
-                        style = foreverTypography.bodyMedium,
-                        color = colorResource(id = R.color.paragraph)
-                    )
-                }
+                SummaryContent(summary = generateSummaryUiState.summary, generateSummaryState = generateSummaryUiState.generateSummaryState)
                 Spacer(modifier = Modifier.size(SPACE_BETWEEN_CONTENT_AND_BUTTONS.dp))
                 Row(
                     horizontalArrangement = Arrangement.SpaceBetween,
---
+++ AOS/app/src/main/java/com/fourever/forever/presentation/generatesummary/SummaryContent.kt
@@ -0,0 +1,101 @@
+package com.fourever.forever.presentation.generatesummary
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.dp
+import com.fourever.forever.R
+import com.fourever.forever.presentation.util.UiState
+import com.fourever.forever.ui.theme.foreverTypography
+
+private const val CONTENT_AREA_HEIGHT = 550
+
+@Composable
+fun SummaryContent(summary: String, generateSummaryState: UiState) {
+    when(generateSummaryState) {
+        UiState.Empty -> OnContentEmpty()
+        UiState.Loading -> OnSummaryLoading()
+        UiState.Success -> OnGetSummary(summary = summary)
+        UiState.Failure -> OnFailure()
+    }
+}
+
+@Composable
+private fun OnContentEmpty() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(
+                height = CONTENT_AREA_HEIGHT.dp
+            ),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+    ) {
+        CircularProgressIndicator(
+            modifier = Modifier.width(30.dp),
+            color = colorResource(id = R.color.blue_strong),
+            trackColor = colorResource(id = R.color.gray_medium),
+        )
+    }
+}
+
+@Composable
+private fun OnSummaryLoading() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(
+                height = CONTENT_AREA_HEIGHT.dp
+            ),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+    ) {
+        CircularProgressIndicator(
+            modifier = Modifier.width(30.dp),
+            color = colorResource(id = R.color.blue_strong),
+            trackColor = colorResource(id = R.color.gray_medium),
+        )
+    }
+}
+
+@Composable
+private fun OnGetSummary(summary: String) {
+    Column(
+        modifier = Modifier
+            .height(
+                height = CONTENT_AREA_HEIGHT.dp
+            )
+            .verticalScroll(rememberScrollState())
+    ) {
+        Text(
+            text = summary,
+            style = foreverTypography.bodyMedium,
+            color = colorResource(id = R.color.paragraph)
+        )
+    }
+}
+
+@Composable
+private fun OnFailure() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(
+                height = CONTENT_AREA_HEIGHT.dp
+            ),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+    ) {
+
+    }
+}
