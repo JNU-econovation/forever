049c7239af693dd37dbb36951927a0afe0bad0ad
MinjuKwak01 <mkwak1125@gmail.com>
2025-05-23 21:18:38 +0900

    fix : 10점 만점으로 변경

--- BE/forever/src/main/java/com/example/forever/dto/feedback/FeedbackRequest.java
+++ BE/forever/src/main/java/com/example/forever/dto/feedback/FeedbackRequest.java
@@ -13,7 +13,7 @@ public record FeedbackRequest(
         
         @NotNull(message = "평점은 필수입니다.")
         @Min(value = 1, message = "평점은 최소 1점 이상이어야 합니다.")
-        @Max(value = 5, message = "평점은 최대 5점까지 가능합니다.")
+        @Max(value = 10, message = "평점은 최대 10점까지 가능합니다.")
         Integer rating
 ) {
 }
